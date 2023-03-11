acc4ed6268021da9a617112d8ca07b261bb6ed0e
Bradford <sd_bradford@hotmail.com>
2023-03-11 14:38:44 +0000

    revert attempt

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,7 @@
 print("Hello World!!!")
+<<<<<<< HEAD
 print("Hello World!!!")
 print("Hello World!!!")
 print("Hello World!!!")
+=======
+>>>>>>> parent of 96f1e4e (recommitting the first version after a revert)
