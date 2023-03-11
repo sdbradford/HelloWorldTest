30c83ff133f398c8658c25714896d0868cdaa3a6
Bradford <sd_bradford@hotmail.com>
2023-03-11 14:39:44 +0000

    funky stuff happened in the code

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,4 @@
 print("Hello World!!!")
-<<<<<<< HEAD
 print("Hello World!!!")
 print("Hello World!!!")
-print("Hello World!!!")
-=======
->>>>>>> parent of 96f1e4e (recommitting the first version after a revert)
+
